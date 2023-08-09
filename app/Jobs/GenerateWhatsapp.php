b85fe76f0b73d478572aa1f413bf2bf643fb59dc
RickySBahaso <ricky@sali-evo.com>
2023-08-09 13:25:35 +0700

    phone number in index

--- app/Jobs/GenerateWhatsapp.php
+++ app/Jobs/GenerateWhatsapp.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\File;
 use App\Models\Contact;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str; 
 
 class GenerateWhatsapp implements ShouldQueue
 {
@@ -39,16 +40,29 @@ class GenerateWhatsapp implements ShouldQueue
         if (config('wahana.workplace') == 'server')
         {
             $waFile = '/var/www/wahanatatar.com/partials/whatsapp-button.php';
+            $configfile = '/var/www/wahanatatar.com/config/phonenumber.php';
         } else 
         {
             $waFile = '/Users/rickysutanto/Development/Laravel/wahanatatar3/partials/whatsapp-button.php';
+            $configfile = '/Users/rickysutanto/Development/Laravel/wahanatatar3/config/phonenumber.php';
         }
         
         $contact = Contact::where('active', 1)->first();
         if ($contact){
-            $button = View::make('partials.whatsapp')->with('number', $contact->number)->render();
-        
+            $number = Str::replace(' ', '', trim($contact->number));
+            $wano = $number;	
+            if (($number[0]=="+")){
+                $wano = '62' . substr($number,1);
+            }
+            if (($number[0]=="0")){
+                $wano = '62' . substr($number,1);
+            }
+            if (($number[0]=="+")){
+                $wano = substr($number,1);	
+            }  
+            
 
+            $button = View::make('partials.whatsapp')->with('number', $wano)->render();
             Storage::put('partials/whatsapp_button.php', $button);
             if (File::exists($waFile)) {
                 File::delete($waFile);
@@ -57,7 +71,21 @@ class GenerateWhatsapp implements ShouldQueue
             if (copy($fname, $waFile)) {
                 Log::info("File " . $fname ." copy successfully.");
             } else {
-                Log::info("Failed to copy events/index.php.");
+                Log::error("Failed to copy partials/whatsapp_button.php.");
+            }
+
+            $wano2 = '0' . substr($wano,2);
+            $format = chunk_split($wano2, 4, ' ');
+            $config = View::make('config.phonenumber')->with('number', $format)->render();
+            Storage::put('config/phonenumber.php', $config);
+            if (File::exists($configfile)) {
+                File::delete($configfile);
+            }
+            $fname = Storage::path('config/phonenumber.php');
+            if (copy($fname, $configfile)) {
+                Log::info("File " . $fname ." copy successfully.");
+            } else {
+                Log::error("Failed to copy config/phonenumber.php.");
             }
         }
         
